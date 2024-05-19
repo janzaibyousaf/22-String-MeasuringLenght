d9923462020f2eb7026df159a98f884a480af3ac
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-05-19 19:51:04 +0500

    Add files via upload

---
+++ 22-StringMeasuringLength/app.js
@@ -0,0 +1,18 @@
+
+
+var a = "murad".length;
+
+console.log(a);
+
+var b = "Hidayatullah".slice(0,7);
+
+console.log(b);
+
+var c = "MrNaseeb".slice(2);
+
+console.log(c);
+
+var fullname = "janzaib";
+var firstchar = fullname.slice(0,1).toUpperCase();
+var result = firstchar + fullname.slice (1)
+console.log(result)
